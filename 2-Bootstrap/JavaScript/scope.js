b40ead6dd330b6de90859bae00fb2f03b1ec2d7c
AdelAziza <adelazizauseni@gmail.com>
2021-11-30 15:05:17 -0800

    JS file

---
+++ 2-Bootstrap/JavaScript/scope.js
@@ -0,0 +1,9 @@
+// var myVar = 1;
+// let myLet = 2;
+// const myConst = 3;
+
+var myVar = 2;
+function testVar() {
+    var myVar = 1;
+    console.log('Inside the function, myVar is: ', myVar);
+}
