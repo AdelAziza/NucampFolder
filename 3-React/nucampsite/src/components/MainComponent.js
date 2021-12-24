22ea67a5e5eb3c4edf1dd1d34d36f4d4e5ed6719
AdelAziza <adelazizauseni@gmail.com>
2021-12-24 01:59:37 -0800

    React Router Parameters

--- 3-React/nucampsite/src/components/MainComponent.js
+++ 3-React/nucampsite/src/components/MainComponent.js
@@ -6,6 +6,7 @@ import { CAMPSITES } from '../shared/campsites';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Contact from './ContactComponent';
+import CampsiteInfo from'./CampsiteInfoComponent';
 import { COMMENTS } from '../shared/comments';
 import { PARTNERS } from '../shared/partners';
 import { PROMOTIONS } from '../shared/promotions';
@@ -34,7 +35,18 @@ class Main extends Component {
                 
                 />
             );
-        };
+        }
+
+        const CampsiteWithId = ({match}) => {
+            return (
+                <CampsiteInfo 
+                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                />
+            );
+        }; 
+
+       
 
         return (
 
@@ -44,6 +56,7 @@ class Main extends Component {
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
                     <Route exact path='/contactus' component={Contact} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Redirect to='/home' />
                     
                 </Switch>
